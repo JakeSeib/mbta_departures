7086cfe2ec946c2f1712048f3ce2db17ff425af7
JakeSeib <jacob.a.seib@gmail.com>
2020-05-19 09:50:13 -0400

    Get predictions from mbta api
    Get and show in browser predictions for north station from the mbta api

--- mbta_departures/departure_board/urls.py
+++ mbta_departures/departure_board/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'departure_board'
+
 urlpatterns = [
     path('', views.index, name='index'),
 ]
--- mbta_departures/departure_board/views.py
+++ mbta_departures/departure_board/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from django.http import HttpResponse
+import jsonapi_requests
+# import the logging library
+import logging
 
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the departure board index.")
+    api = jsonapi_requests.Api.config({
+        'API_ROOT': 'https://api-v3.mbta.com',
+        'VALIDATE_SSL': False,
+        'TIMEOUT': 1,
+        # 'api_key': 'todo: insert an api_key here',
+    })
+
+    prediction_endpoint = api.endpoint('/predictions')
+    prediction_response = prediction_endpoint.get(params={'filter[stop]': 'place-north'})
+
+    context = {
+    'prediction_data': prediction_response.data,
+    }
+
+    return render(request, 'departure_board/index.html', context)
