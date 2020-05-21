021eef4af78c3ac051067f6a3ebeda628b2f5ccb
JakeSeib <jacob.a.seib@gmail.com>
2020-05-21 16:09:05 -0400

    Sensitive info in env variables

--- mbta_departures/settings.py
+++ mbta_departures/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@$td!t9!s_8-*vn77mwu&%8!(r!$4br0kzch+q0=tami-3b0)j'
+SECRET_KEY = os.environ.get('MY_APP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
