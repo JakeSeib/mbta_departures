9c6b3b3a3ac6cf1bae10a72564269ca279fb926a
JakeSeib <jacob.a.seib@gmail.com>
2020-05-21 13:39:53 -0400

    Spell settings right

--- mbta_departures/wsgi.py
+++ mbta_departures/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mbta_departures.ettings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mbta_departures.settings')
 
 application = get_wsgi_application()
