fe1e23b090a7b97f313fc6e24f5976e4cd095591
JakeSeib <jacob.a.seib@gmail.com>
2020-05-21 00:11:02 -0400

    Add public secret key for mvp

--- mbta_departures/mbta_departures/settings.py
+++ mbta_departures/mbta_departures/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = process.env.SECRET_KEY
-API_KEY = process.env.API_KEY
+SECRET_KEY = '@$td!t9!s_8-*vn77mwu&%8!(r!$4br0kzch+q0=tami-3b0)j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
